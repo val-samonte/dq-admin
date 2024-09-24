b90e84a9d565f8e4a599762c7015d48bef2d13c6
Gel Balano <gel@mashupgarage.com>
2024-09-25 06:50:44 +0800

    Create bp container placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,41 @@
 "use client";
 
+import { Button } from "@headlessui/react"
+import { useSession } from "next-auth/react"
+import Image from 'next/image'
+import styles from "./page.module.css";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <main className="flex items-center justify-center min-h-screen flex-col">
-      <h1 className="text-4xl font-bold mb-4">Content here</h1>
-      {/* <div className="border hover:border-slate-900 rounded">
-        <WalletMultiButton style={{}} />
-      </div> */}
+      {!session && (
+        <h1 className="text-4xl font-bold mb-4">Sign in your wallet</h1>
+      )}
+      {session?.user && (
+        <>
+          <div className="controls">
+            <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
+              Create new blueprint
+            </Button>
+          </div>
+
+          <div className={styles.bpContainer}>
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 inline-flex flex-col">
+
+                <Image className="p-8 rounded-t-lg flex-center self-center justify-self-center" src="/images/Soul_Mantle_inventory_icon.png" alt="product image" width={300} height={500} />
+                <div className="px-5 pb-5">
+                    <h5 className="text-md font-semibold tracking-tight text-gray-900 dark:text-white pb-4">Cleric mantle lorem ipsum.</h5>
+                    <div className="flex items-center justify-between">
+                        <span className="text-xl font-bold text-gray-900 dark:text-white">SpiderSilk Mantle</span>
+                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2">Create Recipe</a>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </>
+      )}
     </main>
   );
 }
