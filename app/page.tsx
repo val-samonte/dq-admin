be7b02b09e585fbf7585453f157d038949489752
Gel Balano <gel@mashupgarage.com>
2024-09-25 07:02:56 +0800

    Fix item card style

--- app/page.tsx
+++ app/page.tsx
@@ -23,15 +23,20 @@ export default function Home() {
 
           <div className={styles.bpContainer}>
             <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 inline-flex flex-col">
-
-                <Image className="p-8 rounded-t-lg flex-center self-center justify-self-center" src="/images/Soul_Mantle_inventory_icon.png" alt="product image" width={300} height={500} />
-                <div className="px-5 pb-5">
-                    <h5 className="text-md font-semibold tracking-tight text-gray-900 dark:text-white pb-4">Cleric mantle lorem ipsum.</h5>
-                    <div className="flex items-center justify-between">
-                        <span className="text-xl font-bold text-gray-900 dark:text-white">SpiderSilk Mantle</span>
-                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2">Create Recipe</a>
-                    </div>
+                <a href="#" className="flex flex-col">
+                  <Image className="p-8 rounded-t-lg flex-center self-center justify-self-center" src="/images/Soul_Mantle_inventory_icon.png" alt="product image" width={300} height={500} />
+                </a>
+              <div className="px-5 pb-5">
+                <a href="#">
+                  <h5 className="text-md font-semibold tracking-tight text-gray-900 dark:text-white pb-4">Cleric mantle lorem ipsum.</h5>
+                </a>
+                <div className="flex items-center justify-between">
+                    <a href="#">
+                      <span className="text-xl font-bold text-gray-900 dark:text-white">SpiderSilk Mantle</span>
+                    </a>
+                    <Button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-2">Create Recipe</Button>
                 </div>
+              </div>
             </div>
           </div>
         </>
