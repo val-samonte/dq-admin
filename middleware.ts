c94c9636ce39f692502eaa6f75df003547682e75
Gel Balano <gel@mashupgarage.com>
2024-09-25 17:45:46 +0800

    Fix redirect

--- middleware.ts
+++ middleware.ts
@@ -10,23 +10,21 @@ async function getSessionToken(req: NextRequest) {
   return token
 }
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
 
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
   // const cookie = request.cookies.get('walletAddress')
-
-  getSessionToken(request).then(token => {
-    if (token) {
-      // User is authenticated, access session data here
-      const walletAddress = token.sub // Wallet PubKey (from NextAuth token)
-      const { pathname } = request.nextUrl
-      console.log('request', pathname)
-      if (walletAddress && pathname == "/") {
-        return NextResponse.redirect(new URL(`/${walletAddress}/blueprints`, request.url));
-      }
+  const token = await getSessionToken(request)
+  if (token) {
+    // User is authenticated, access session data here
+    const walletAddress = token.sub // Wallet PubKey (from NextAuth token)
+    const { pathname } = request.nextUrl
+    if (walletAddress && pathname == "/") {
+      console.log('DO REDIRECT', pathname, `/${walletAddress}/blueprints`)
+      return NextResponse.redirect(new URL(`/${walletAddress}/blueprints`, request.url));
     }
-  })
+  }
 
   return NextResponse.next()
 }
