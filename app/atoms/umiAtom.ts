32cdf69f0733ac38087da1e47fe3f04709842a77
Gel Balano <gel@mashupgarage.com>
2024-10-01 02:44:42 +0800

    Use wallet atom

--- app/atoms/umiAtom.ts
+++ app/atoms/umiAtom.ts
@@ -3,10 +3,10 @@ import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters'
 import { mplCore } from '@metaplex-foundation/mpl-core'
 import { irysUploader } from '@metaplex-foundation/umi-uploader-irys'
-import { useWallet } from '@solana/wallet-adapter-react'
+import { userWalletAtom } from './userWalletAtom'
 
-export const umiAtom = atom(() => {
-  const wallet = useWallet()
+export const umiAtom = atom((get) => {
+  const wallet = get(userWalletAtom)
   const umi = createUmi('https://api.devnet.solana.com')
   umi.use(mplCore())
   umi.use(irysUploader({address: "https://devnet.irys.xyz"}))
